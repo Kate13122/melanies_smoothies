07f4e77f7c86df4abe7ff6bda3abf4a92711b4e2
Kate13122 <119351271+Kate13122@users.noreply.github.com>
2025-01-13 14:12:06 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -40,9 +40,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
+   #API connection      
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
 
@@ -58,7 +57,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!',icon='✅')
 
-#API connection 
+
 
 
    
