6911ec79d47d21ef7ecd997c3bbd169001a3232a
Kate13122 <119351271+Kate13122@users.noreply.github.com>
2025-01-13 16:15:39 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -28,7 +28,7 @@ from snowflake.snowpark.functions import col
 
 #session = get_active_session()
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col ('SEARCH ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col ('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 ST.STOP()
 
