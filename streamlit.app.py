3f66434c3f6f8428a77629d3001a2ddb297f4c2e
Kate13122 <119351271+Kate13122@users.noreply.github.com>
2025-01-13 15:51:43 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,9 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 cnx = st.connection("snowflake")
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 
 
@@ -54,6 +52,9 @@ if ingredients_list:
             values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
     st.write(my_insert_stmt)
     #st.stop()
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
