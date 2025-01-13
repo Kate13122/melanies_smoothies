864d59bb4391c9a740a67753c82798d5007f1b44
Kate13122 <119351271+Kate13122@users.noreply.github.com>
2025-01-13 15:28:26 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -42,18 +42,23 @@ ingredients_list = st.multiselect( 'Choose up to 5 ingredients:',
                                    my_dataframe
                                   ,max_selections = 5)
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    
-        ingredients_string = ''
-        for fruit_chosen in ingredients_list:
-                ingredients_string += fruit_chosen + ' '
-                search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-   #API connection
-            st.subheader(fruit_chosen + ' Nutrition Information')
-            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-            sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        
+        # Correct indentation here
+        st.write(f"The search value for {fruit_chosen} is {search_on}.")
+        st.subheader(f'{fruit_chosen} Nutrition Information')  # This line is properly indented
+        
+        # Make the API call for each selected fruit
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+        
+        if smoothiefroot_response.status_code == 200:
+            sf_df = smoothiefroot_response.json()
+            st.dataframe(data=sf_df, use_container_width=True)
+        else:
+            st.write("Error: Nutrition information not found.")
 
 
 
